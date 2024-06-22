c950b42e710feb7719a61fc5723dd546ec74c395
Timothy Marois <timothymarois@gmail.com>
2024-06-22 17:30:33 -0400

    Update for inertia setup

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <Head title="Home" />
+    <h1 class="mb-8 text-3xl font-bold">Home</h1>
+    <p class="mb-8 leading-normal">This is a home page.</p>
+  </div>
+</template>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html class="h-full bg-gray-100">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg">
+        @vite('resources/js/app.js')
+        @inertiaHead
+    </head>
+    <body class="font-sans leading-none text-gray-700 antialiased">
+        @inertia
+    </body>
+</html>
