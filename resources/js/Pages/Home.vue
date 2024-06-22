a60f5e8eab93fc85c206f2dec231de64bb333314
Timothy Marois <timothymarois@gmail.com>
2024-06-22 18:15:57 -0400

    Added tailwindcss

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -2,6 +2,6 @@
   <div>
     <Head title="Home" />
     <h1 class="mb-8 text-3xl font-bold">Home</h1>
-    <p class="mb-8 leading-normal">This is a home page.</p>
+    <p class="mb-8 leading-normal text-red-500">This is a home page.</p>
   </div>
 </template>
