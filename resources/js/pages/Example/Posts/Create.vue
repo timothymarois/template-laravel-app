62f0eaae9ee6418c159414138499ad4291d0a611
Timothy Marois <timothymarois@gmail.com>
2024-06-25 17:22:43 -0400

    added errors to form examples

--- resources/js/pages/Example/Posts/Create.vue
+++ resources/js/pages/Example/Posts/Create.vue
@@ -12,10 +12,12 @@
                 <div class="mb-4">
                     <label for="title" class="block text-gray-700 text-sm font-bold mb-2">Title:</label>
                     <input v-model="form.title" id="title" type="text" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Title">
+                    <div v-if="form.errors.title" class="text-red-600 text-sm">{{ form.errors.title  }}</div>
                 </div>
                 <div class="mb-4">
                     <label for="body" class="block text-gray-700 text-sm font-bold mb-2">Body:</label>
                     <textarea v-model="form.body" id="body" rows="5" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Body"></textarea>
+                    <div v-if="form.errors.body" class="text-red-600 text-sm">{{ form.errors.body  }}</div>
                 </div>
                 <div class="flex items-center justify-between">
                     <Button @click="submitForm" :disabled="form.processing" :loading="form.processing" label="Update" />
