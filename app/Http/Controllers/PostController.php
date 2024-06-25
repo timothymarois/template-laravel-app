e9d96544b3bf8ab8dc3e0b8fb7d716d89d3b6cc3
Timothy Marois <timothymarois@gmail.com>
2024-06-24 21:19:41 -0400

    Update example

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,7 +11,7 @@ class PostController extends Controller
     public function index()
     {
         $posts = Post::query();
-        return response()->inertiaOrJson('Posts/Index', $posts->paginate());
+        return response()->inertiaOrJson('Posts/Index', ['posts' => $posts->paginate()]);
     }
 
     public function create()
