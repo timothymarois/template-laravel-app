c59ac77116b69bfb97a67c505b27b80c02c03b00
Timothy Marois <timothymarois@gmail.com>
2024-06-27 08:11:00 -0400

    Update routes for middleware guest

--- routes/web.php
+++ routes/web.php
@@ -7,20 +7,16 @@ use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PageController::class, 'index'])->name('index');
-
-/**
- * Auth:
- * - Register
- * - Login
- */
-Route::get('register', [RegisterController::class, 'registerView'])->name('register');
-Route::get('login', [SessionController::class, 'loginView'])->name('login');
-Route::post('auth/register', [RegisterController::class, 'store'])->name('auth.register');
-Route::post('auth/login', [SessionController::class, 'authenticate'])->name('auth.login');
-Route::get('logout', [SessionController::class, 'destroy'])->name('auth.logout');
-
-/**
- * Example routes
- */
 Route::get('/example/store', [PageController::class, 'exampleStore'])->name('example.store');
-Route::resource('/admin/users', UserController::class)->middleware('auth:sanctum');
+
+Route::middleware(['guest'])->group(function () {
+    Route::get('register', [RegisterController::class, 'registerView'])->name('register');
+    Route::get('login', [SessionController::class, 'loginView'])->name('login');
+    Route::post('auth/register', [RegisterController::class, 'store'])->name('auth.register');
+    Route::post('auth/login', [SessionController::class, 'authenticate'])->name('auth.login');
+});
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::resource('/admin/users', UserController::class);
+    Route::get('logout', [SessionController::class, 'destroy'])->name('auth.logout');
+});
