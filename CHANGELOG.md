602627b9ca2feae46b12b6db295141d71a78f45e
Timothy Marois <timothymarois@gmail.com>
2024-07-27 11:57:33 -0400

    Update to primevue 4 config/theme

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,3 +3,17 @@
 This file lists the changes that are made with this template and how to migrate any projects that use it. 
 
 ## Unreleased
+
+## 07/27/2024
+
+PrimeVue 3 to PrimeVue 4
+
+1. `npm remove primevue`
+2. `npm install -D primevue`
+3. `npm i -D tailwindcss-primeui`
+4. Add `require('tailwindcss-primeui')` to tailwind plugins config.
+5. Replace the `resources/css/theme.css` file
+6. Download the [v4 presets](https://github.com/primefaces/primevue-tailwind/releases)
+7. Copy or replace them into `resources/presets`
+8. Remove all the custom attributes in tailwind config theme `colors`.
+9. If you made custom theming, you will need to update those.
--- resources/js/app.js
+++ resources/js/app.js
@@ -10,6 +10,135 @@ import { createPersistedState } from 'pinia-plugin-persistedstate'
 
 import PrimeVue from 'primevue/config';
 import Lara from '/resources/presets/lara';
+// import Aura from '/resources/presets/aura';
+
+/*
+
+ALL PRIMEVUE COMPONENTS
+If you need to manual import, use one of these in your template
+
+import AutoComplete from 'primevue/autocomplete';
+import Accordion from 'primevue/accordion';
+import AccordionPanel from 'primevue/accordionpanel';
+import AccordionHeader from 'primevue/accordionheader';
+import AccordionContent from 'primevue/accordioncontent';
+import AccordionTab from 'primevue/accordiontab';
+import AnimateOnScroll from 'primevue/animateonscroll';
+import Avatar from 'primevue/avatar';
+import AvatarGroup from 'primevue/avatargroup';
+import Badge from 'primevue/badge';
+import BadgeDirective from "primevue/badgedirective";
+import BlockUI from 'primevue/blockui';
+import Button from 'primevue/button';
+import ButtonGroup from 'primevue/buttongroup';
+import Breadcrumb from 'primevue/breadcrumb';
+import Calendar from 'primevue/calendar';
+import Card from 'primevue/card';
+import CascadeSelect from 'primevue/cascadeselect';
+import Carousel from 'primevue/carousel';
+import Checkbox from 'primevue/checkbox';
+import Chip from 'primevue/chip';
+import Chips from 'primevue/chips';
+import ColorPicker from 'primevue/colorpicker';
+import Column from 'primevue/column';
+import ColumnGroup from 'primevue/columngroup';
+import ConfirmDialog from 'primevue/confirmdialog';
+import ConfirmPopup from 'primevue/confirmpopup';
+import ConfirmationService from 'primevue/confirmationservice';
+import ContextMenu from 'primevue/contextmenu';
+import DataTable from 'primevue/datatable';
+import DataView from 'primevue/dataview';
+import DatePicker from 'primevue/datepicker';
+import DeferredContent from 'primevue/deferredcontent';
+import Dialog from 'primevue/dialog';
+import DialogService from 'primevue/dialogservice'
+import Divider from 'primevue/divider';
+import Dock from 'primevue/dock';
+import Drawer from 'primevue/drawer';
+import Dropdown from 'primevue/dropdown';
+import DynamicDialog from 'primevue/dynamicdialog';
+import Fieldset from 'primevue/fieldset';
+import FileUpload from 'primevue/fileupload';
+import FloatLabel from 'primevue/floatlabel';
+import Fluid from 'primevue/fluid';
+import FocusTrap from 'primevue/focustrap';
+import Galleria from 'primevue/galleria';
+import IconField from 'primevue/iconfield';
+import InputIcon from 'primevue/inputicon';
+import Image from 'primevue/image';
+import InlineMessage from 'primevue/inlinemessage';
+import Inplace from 'primevue/inplace';
+import InputChips from 'primevue/inputchips';
+import InputGroup from 'primevue/inputgroup';
+import InputGroupAddon from 'primevue/inputgroupaddon';
+import InputMask from 'primevue/inputmask';
+import InputNumber from 'primevue/inputnumber';
+import InputOtp from 'primevue/inputotp';
+import InputSwitch from 'primevue/inputswitch';
+import InputText from 'primevue/inputtext';
+import Knob from 'primevue/knob';
+import Listbox from 'primevue/listbox';
+import MegaMenu from 'primevue/megamenu';
+import Menu from 'primevue/menu';
+import Menubar from 'primevue/menubar';
+import Message from 'primevue/message';
+import MeterGroup from 'primevue/metergroup';
+import MultiSelect from 'primevue/multiselect';
+import OrderList from 'primevue/orderlist';
+import OrganizationChart from 'primevue/organizationchart';
+import OverlayBadge from 'primevue/overlaybadge';
+import OverlayPanel from 'primevue/overlaypanel';
+import Paginator from 'primevue/paginator';
+import Panel from 'primevue/panel';
+import PanelMenu from 'primevue/panelmenu';
+import Password from 'primevue/password';
+import PickList from 'primevue/picklist';
+import Popover from 'primevue/popover';
+import ProgressBar from 'primevue/progressbar';
+import ProgressSpinner from 'primevue/progressspinner';
+import Rating from 'primevue/rating';
+import RadioButton from 'primevue/radiobutton';
+import Ripple from 'primevue/ripple';
+import Row from 'primevue/row';
+import ScrollPanel from 'primevue/scrollpanel';
+import ScrollTop from 'primevue/scrolltop';
+import Select from 'primevue/select';
+import SelectButton from 'primevue/selectbutton';
+import Skeleton from 'primevue/skeleton';
+import Slider from 'primevue/slider';
+import Sidebar from 'primevue/sidebar';
+import SpeedDial from 'primevue/speeddial';
+import SplitButton from 'primevue/splitbutton';
+import Splitter from 'primevue/splitter';
+import SplitterPanel from 'primevue/splitterpanel';
+import Stepper from 'primevue/stepper';
+import StepperPanel from 'primevue/stepperpanel';
+import Steps from 'primevue/steps';
+import StyleClass from 'primevue/styleclass';
+import TabMenu from 'primevue/tabmenu';
+import Tabs from 'primevue/tabs';
+import TabList from 'primevue/tablist';
+import Tab from 'primevue/tab';
+import TabPanels from 'primevue/tabpanels';
+import TabPanel from 'primevue/tabpanel';
+import TabView from 'primevue/tabview';
+import Tag from 'primevue/tag';
+import Terminal from 'primevue/terminal';
+import Textarea from 'primevue/textarea';
+import TieredMenu from 'primevue/tieredmenu';
+import Timeline from 'primevue/timeline';
+import ToggleButton from 'primevue/togglebutton';
+import ToggleSwitch from 'primevue/toggleswitch';
+import Tooltip from 'primevue/tooltip';
+import Toast from 'primevue/toast';
+import ToastService from 'primevue/toastservice';
+import Toolbar from 'primevue/toolbar';
+import Tree from 'primevue/tree';
+import TreeSelect from 'primevue/treeselect';
+import TreeTable from 'primevue/treetable';
+import VirtualScroller from 'primevue/virtualscroller';
+
+*/
 
 createInertiaApp({
     resolve: async (name) => {
@@ -42,13 +171,7 @@ createInertiaApp({
             .use(PrimeVue, {
                 ripple: true,
                 unstyled: true,
-                pt: Lara,
-                components: {
-                    exclude: [
-                        "Editor",
-                        "Chart"
-                    ]
-                }
+                pt: Lara
             })
             .component('Link', Link)
             .component('Head', Head);
