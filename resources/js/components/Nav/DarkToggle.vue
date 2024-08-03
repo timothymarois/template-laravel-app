28883a1ed84e77412e0e5a6e7b5fc61f456e4c99
Timothy Marois <timothymarois@gmail.com>
2024-08-02 21:08:03 -0400

    eslint fix

--- resources/js/components/Nav/DarkToggle.vue
+++ resources/js/components/Nav/DarkToggle.vue
@@ -1,7 +1,7 @@
 <template>
     <button type="button"
-        class="h-8 w-8 rounded-lg inline-flex justify-center items-center bg-surface-100 dark:bg-surface-800 hover:bg-surface-700 dark:hover:bg-surface-700 text-surface-600 hover:text-surface-900 dark:text-surface-300 dark:hover:text-surface-200 transition-colors duration-200"
-        @click="themeStore.toggleDark">
+            class="h-8 w-8 rounded-lg inline-flex justify-center items-center bg-surface-100 dark:bg-surface-800 hover:bg-surface-700 dark:hover:bg-surface-700 text-surface-600 hover:text-surface-900 dark:text-surface-300 dark:hover:text-surface-200 transition-colors duration-200"
+            @click="themeStore.toggleDark">
         <i v-if="themeStore.isDark" class="pi pi-moon" />
         <i v-else class="pi pi-sun" />
     </button>
--- resources/js/components/Nav/Nav.vue
+++ resources/js/components/Nav/Nav.vue
@@ -8,9 +8,9 @@
                     </div>
                     <div class="hidden md:block">
                         <div class="ml-10 flex items-baseline space-x-4">
-                        <Link :class="{'bg-surface-900 text-white' : $page.url === '/' }" href="/" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white" aria-current="page">Home</Link>
-                        <Link :class="{'bg-surface-900 text-white' : $page.url.startsWith('/admin/users') }" href="/admin/users" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Users</Link>
-                        <Link :class="{'bg-surface-900 text-white' : $page.url.startsWith('/example/store') }" href="/example/store" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Store</Link>
+                            <Link :class="{'bg-surface-900 text-white' : $page.url === '/' }" href="/" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white" aria-current="page">Home</Link>
+                            <Link :class="{'bg-surface-900 text-white' : $page.url.startsWith('/admin/users') }" href="/admin/users" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Users</Link>
+                            <Link :class="{'bg-surface-900 text-white' : $page.url.startsWith('/example/store') }" href="/example/store" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Store</Link>
                         </div>
                     </div>
                 </div>
