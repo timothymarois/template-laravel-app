db08de436680c81bdb53d91c6988b3dd29146e87
Tim Marois <timothymarois@gmail.com>
2025-05-17 15:01:30 -0400

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,4 @@
-import pluginVue from 'eslint-plugin-vue'
+import pluginVue from 'eslint-plugin-vue';
 export default [
     ...pluginVue.configs['flat/recommended'],
     {
@@ -6,17 +6,16 @@ export default [
             'vue/attribute-hyphenation': 'off',
             'vue/no-unused-vars': 'warn',
             'vue/max-attributes-per-line': ['error', { singleline: 5, multiline: 1 }],
-            'vue/multi-word-component-names': "off",
+            'vue/multi-word-component-names': 'off',
             'vue/singleline-html-element-content-newline': 'off',
             'vue/multi-word-component-names': 'off',
             'vue/html-indent': [
-                "error",
-                4, {
-                    "attribute": 1,
-                    "baseIndent": 1,
-                    "closeBracket": 0,
-                    "alignAttributesVertically": true,
-                    "ignores": []
+                'error', 4, {
+                    attribute: 1,
+                    baseIndent: 1,
+                    closeBracket: 0,
+                    alignAttributesVertically: true,
+                    ignores: []
                 }
             ],
             'vue/html-closing-bracket-newline': [
