93dfa2a4f7d760dedafd7df14a26e8d321d7c9ee
Tim Marois <timothymarois@gmail.com>
2025-08-30 14:39:35 -0400

    Added port for inertia ssr

--- resources/js/ssr.js
+++ resources/js/ssr.js
@@ -2,16 +2,22 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import createServer from '@inertiajs/vue3/server';
 import { renderToString } from '@vue/server-renderer';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { setupApp } from '../../resources/js/setup';
+import { setupApp } from './setup';
 
-createServer((page) =>
-    createInertiaApp({
-        page,
-        render: renderToString,
-        resolve: (name) => resolvePageComponent(`./pages/${name}.vue`, import.meta.glob('../../resources/js/pages/**/*.vue')),
-        title: title => title ?? '',
-        setup: async ({ App, props, plugin }) => {
-            return await setupApp({ App, props, plugin, isServer: true });
-        },
-    })
+const PORT = Number(process.env.INERTIA_SSR_PORT || '13714');
+const HOST = process.env.INERTIA_SSR_HOST || '127.0.0.1';
+
+createServer(
+    (page) =>
+        createInertiaApp({
+            page,
+            render: renderToString,
+            resolve: (name) => resolvePageComponent(`./pages/${name}.vue`, import.meta.glob('./pages/**/*.vue')),
+            title: (title) => title ?? '',
+            setup: async ({ App, props, plugin }) => {
+                return await setupApp({ App, props, plugin, isServer: true });
+            },
+        }),
+    PORT,
+    HOST
 );
