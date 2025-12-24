13cfb39022959c84272fa1517f62744d98296468
timothymarois <timothymarois@gmail.com>
2025-12-23 21:01:22 -0500

    Remove unused Tailwind CSS plugins
    Removed @tailwindcss/forms and @tailwindcss/container-queries from devDependencies and Tailwind config as they are no longer used. Updated package.json, pnpm-lock.yaml, and tailwind.config.js accordingly.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,6 @@ export default {
     content: [
         './resources/js/**/*.{js,ts,vue}',
     ],
-    plugins: [
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/typography'),
-    ],
     darkMode: 'false',
     theme: {
         extend: {
